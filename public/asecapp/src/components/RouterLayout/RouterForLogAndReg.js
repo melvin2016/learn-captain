259f554579c04c7c85f0b522e072e94fdc3c0bde
Melvin <melvingeorge10@gmail.com>
2018-07-06 00:20:29 +0530

    added state for forms in register contoller

--- public/asecapp/src/components/RouterLayout/RouterForLogAndReg.js
+++ public/asecapp/src/components/RouterLayout/RouterForLogAndReg.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Route , Switch} from 'react-router-dom';
 import Login from '../loginAndRegister/login/Login';
 import Register from '../loginAndRegister/register/Register';
 
-const RouterForLogAndReg = ()=>{
-    return (
-        <Switch>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-        </Switch>
-    );
+class RouterForLogAndReg extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isRegistered : false,
+            isLoggedIn : false,
+            regInputs:{
+                userid:null,
+                password:null,
+                cPassword:null,
+                email:null,
+                tel:null,
+                uniseat:null
+            }
+        }
+    }
+    submitHandlerReg = ()=>{
+        console.log(this.state);
+    }
+    handleInput=(e)=>{
+        const regInputs = {...this.state.regInputs}
+        const regInputTarget = e.target.id;
+        const regInputValue = e.target.value;
+        switch(regInputTarget){
+            case 'userid' : this.setState({regInputs:{...regInputs,userid:regInputValue}});
+            break;
+            case 'password' : this.setState({regInputs:{...regInputs,password:regInputValue}});
+            break;
+            case 'cPassword' : this.setState({regInputs:{...regInputs,cPassword:regInputValue}});
+            break;
+            case 'email' : this.setState({regInputs:{...regInputs,email:regInputValue}});
+            break;
+            case 'tel' : this.setState({regInputs:{...regInputs,tel:regInputValue}});
+            break;
+            case 'uniseat' : this.setState({regInputs:{...regInputs,uniseat:regInputValue}});
+            break;
+            default : throw new Error("Thing Which is not in e.target");
+        }
+    }
+    render(){
+        return (
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/register" render={ 
+                    /*Giving Out Props to Route */
+                    (props) => 
+                    <Register {...props} submitHandlerReg={this.submitHandlerReg} handleInput={this.handleInput}/> 
+                }/>
+            </Switch>
+        );
+    }
 };
 export default RouterForLogAndReg;
--- public/asecapp/src/components/loginAndRegister/register/Register.js
+++ public/asecapp/src/components/loginAndRegister/register/Register.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import classes from './Register.css';
 import {Row,Input,Icon,Button} from 'react-materialize';
-const Register = ()=>{
+const Register = (props)=>{
+    console.log(props);
     return(
         <div className={classes.nav+" container"}>
             <h1 className="flow-text center-align">Register</h1>
-            <Row><Input s={12} m={12} label="User Id" validate><Icon>person</Icon></Input></Row>
+            <Row><Input s={12} m={12} label="User Id" id="userid"validate onChange={props.handleInput}><Icon>person</Icon></Input></Row>
             <Row>
-                <Input s={7} m={7} label="Password" validate><Icon>lock</Icon></Input>
-                <Input s={5} m={5} label="Confirm password" validate></Input>
+                <Input s={7} m={7} label="Password" id="password" type="password" validate onChange={props.handleInput}><Icon>lock</Icon></Input>
+                <Input s={5} m={5} label="Confirm password" id="cPassword" type="password" validate onChange={props.handleInput}></Input>
                 </Row>
-            <Row><Input s={12} m={12} label="Email Id" validate><Icon>email</Icon></Input></Row>
-            <Row><Input s={12} m={12} label="Mobile Number" validate><Icon>contact_phone</Icon></Input></Row>
-            <Row><Input s={12} m={12} label="University Roll No" validate><Icon>create</Icon></Input></Row>
-            <Row><Button className={classes.submit} waves='light'>Register<Icon right>chevron_right</Icon></Button></Row>
+            <Row><Input s={12} m={12} label="Email Id" type="email" id="email" validate onChange={props.handleInput}><Icon>email</Icon></Input></Row>
+            <Row><Input s={12} m={12} label="Mobile Number" type="tel" id="tel" validate onChange={props.handleInput}><Icon>contact_phone</Icon></Input></Row>
+            <Row><Input s={12} m={12} label="University Roll No" id="uniseat" validate onChange={props.handleInput}><Icon>create</Icon></Input></Row>
+            <Row><Button className={classes.submit} waves='light' onClick={props.submitHandlerReg}>Register<Icon right>chevron_right</Icon></Button></Row>
         </div>
     );
 };
